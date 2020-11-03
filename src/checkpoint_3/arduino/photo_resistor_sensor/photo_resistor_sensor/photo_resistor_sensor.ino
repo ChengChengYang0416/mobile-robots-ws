391441c45a1bc8d3e98d35ce5751518db366e7bd
ChengChengYang1997 <kb240608@gmail.com>
2020-11-03 15:49:37 +0800

    add arduino package of checkpoint_3

---
+++ src/checkpoint_3/arduino/photo_resistor_sensor/photo_resistor_sensor/photo_resistor_sensor.ino
@@ -0,0 +1,13 @@
+#define photo_sensor_pin A1
+
+int val = 0;
+
+void setup(){
+  pinMode(photo_sensor_pin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  val = analogRead(photo_sensor_pin);
+  Serial.println(val);
+}
---
+++ src/checkpoint_3/arduino/touch_sensor/touch_sensor/touch_sensor.ino
@@ -0,0 +1,17 @@
+#define touch_pin_L 12  // connected to mid pin of touch sensor
+#define touch_pin_R 13  // connected to mid pin of touch sensor
+#define touch_pin_M A0  // connected to mid pin of touch sensor
+
+void setup(){
+    pinMode(touch_pin_L, INPUT);
+    pinMode(touch_pin_R, INPUT);
+    pinMode(touch_pin_M, INPUT);
+    Serial.begin(9600);
+}
+
+void loop(){
+    if (digitalRead(touch_pin_M) == HIGH){
+        Serial.println("touched");
+        delay(500);
+    }
+}
