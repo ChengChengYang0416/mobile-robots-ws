0b4d2ac740a86b4725892785e2a0534124572a0c
ChengChengYang1997 <kb240608@gmail.com>
2020-11-17 11:28:10 +0800

    rearrange the package

--- build/checkpoint_3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ncrl/workspace/ros_workspace/mobile_robots_ws/src/checkpoint_3
-# Build directory: /home/ncrl/workspace/ros_workspace/mobile_robots_ws/build/checkpoint_3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
