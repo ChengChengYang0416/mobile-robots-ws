7ccba08a37a9b8aa5d0fd0cd96102f180ee283fd
ChengChengYang1997 <kb240608@gmail.com>
2020-12-02 20:24:30 +0800

    add example for IR

---
+++ checkpoint_4/arduino/ir_example/ir_example.ino
@@ -0,0 +1,21 @@
+#include <Timer.h>
+#include <Event.h>
+
+#include <IRremote.h>
+
+const int RECV_PIN = A2;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+}
+
+void loop(){
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+  }
+}
