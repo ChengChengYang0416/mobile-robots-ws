fba77d132b79fe84a9806580f4c28ad208d1255e
Cheng-Cheng Yang <kb240608@gmail.com>
2020-12-11 11:30:50 +0800

    decrease the duration for initial moving forward

--- robot_contest/arduino/robot_contest/robot_contest.ino
+++ robot_contest/arduino/robot_contest/robot_contest.ino
@@ -280,7 +280,7 @@ void loop()
     motor_forward_PID();
     pid_counter++;
     //Serial.println(pid_counter);
-    if(pid_counter >= 300){  // 300, 200, 800
+    if(pid_counter >= 250){  // 300, 200, 800
       first_move = 0;
     }
   }else{
